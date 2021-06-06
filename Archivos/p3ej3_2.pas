2018fe0de5b3123bed085856bf8c749a4676b9f3
Gregorio Maclen <gregorio.maclen@gmail.com>
2021-06-06 16:48:16 -0300

    Agrego buen ejemplo de lista invertida

--- Archivos/p3ej3_2.pas
+++ Archivos/p3ej3_2.pas
@@ -1,4 +1,5 @@
-program p3ej3_2;
+//solo la parte de bajas
+program p3ej3_2; //creo que esta bien. esta mejor que la primera version de p3ej3
 const
 valoralto = 9999;
 type
@@ -8,49 +9,50 @@ regNovela = record
   duracion,precio:real;
 end;
 bin = file of regNovela;
-procedure
+procedure realizarBaja (var archivo:bin; codigo:integer);
   var
-
+    novela,cabecera:regNovela;
   begin
-
+    read(archivo,novela);
+    cabecera:=novela;
+    while (not eof(archivo)) and (novela.codigo <> codigo) do
+      read(archivo,novela);
+    if (novela.codigo = codigo) then begin
+      novela:=cabecera;
+      seek(archivo,filepos(archivo)-1);
+      write(archivo,novela);
+      cabecera.codigo:=filepos(archivo) * -1;
+      seek(archivo,0);
+      write(archivo,cabecera);
+    end;
   end;
-procedure
+procedure agregar (var archivo:bin; novela:regNovela);
   var
-
+    cabecera,actual:regNovela;
   begin
-
-  end;
-procedure
-  var
-
-  begin
-
-  end;
-procedure
-  var
-
-  begin
-
-  end;
-procedure
-  var
-
-  begin
-
-  end;
-procedure desplegarMenu ();
-  var
-    opcion:integer;
-  begin
-    opcion:=1;
-    while (opcion <> 0) do begin
-      case opcion of:
-        1:
-      write('Ingrese la opcion: ');readln(opcion);
+    read(archivo,cabecera);
+    if (cabecera.codigo = 0) then begin //no hay espacio libre
+      seek(archivo,filesize(archivo)-1);
+      write(archivo,novela);
+    end
+    else begin
+      seek(archivo,cabecera.codigo * -1); //voy a la posicion libre
+      read(archivo,actual);
+      cabecera:=actual; //me guardo el siguiente enlace
+      seek(archivo,filepos(archivo)-1);
+      write(archivo,novela); //escribo el registro nuevo
+      seek(archivo,0);
+      write(archivo,cabecera); //actualizo la cabecera
     end;
   end;
 var
-
+  codigo:integer; archivo:bin; novela:regNovela;
 begin
-
+  assign(archivo,'p3ej2_2.dat');
+  rewrite(archivo);
+  write('Ingrese el codigo: ');readln(codigo);
+  realizarBaja(archivo,codigo)
+  leerNovela(novela); //se dispone
+  agregar(archivo,novela);
+  close(archivo);
 end.
